5bf566e167c761d984a6038a5d6cbf1ddc8825fc
ghaaj <91141283+ghaaj@users.noreply.github.com>
2026-03-19 23:48:16 +0900

    Move pane initialization out of p.setup

--- src/main.ts
+++ src/main.ts
@@ -59,111 +59,111 @@ new p5((p: p5) => {
   let isPlaying = false;
   let isReset = true;
 
+  const locus: Point2D[] = [];
+  function setIsReset(value: boolean) {
+    isReset = value && !isPlaying;
+    bcFolder.disabled = !isReset;
+    if (value) locus.length = 0;
+  }
+
+  const pane = new Pane();
+  const materialFolder = pane.addFolder({ title: "Material", expanded: false });
+  materialFolder.addBinding(baseParams, "l", { min: 0 });
+  materialFolder.addBinding(baseParams, "m", { min: 0 });
+  materialFolder.addBinding(baseParams, "M", { min: 0 });
+
+  const simFolder = pane.addFolder({ title: "Simulation", expanded: false });
+  simFolder.addBinding(baseParams, "dt", { min: 0, max: 1 });
+  simFolder.addBinding(baseParams, "$gravity", { label: "Gravity" });
+  simFolder.addBinding(baseParams, "showLocus", { label: "Show Locus" });
+
+  function initializeHamiltonianMonitor() {
+    const hamiltonianInit = simulator.renderableState.hamiltonian;
+    const { valueController: { props }, value: { rawValue } } = hamiltonianMonitor.controller;
+    props.set("min", hamiltonianInit * 0.9);
+    props.set("max", hamiltonianInit * 1.1);
+    rawValue.fill(undefined);
+  }
+  const hamiltonianOpts = {
+    enabled: true,
+    get hamiltonian() {
+      return simulator.renderableState.hamiltonian;
+    },
+  };
+  simFolder.addBinding(hamiltonianOpts, "enabled", { label: "Monitor Hamiltonian" }).on(
+    "change",
+    () => {
+      hamiltonianMonitor.hidden = !hamiltonianOpts.enabled;
+    },
+  );
+  const hamiltonianMonitor = simFolder.addBinding(hamiltonianOpts, "hamiltonian", {
+    label: "Hamiltonian",
+    readonly: true,
+    view: "graph",
+  }) as GraphLogMonitorBindingApi;
+  initializeHamiltonianMonitor();
+
+  const bcFolder = pane.addFolder({ title: "Boundary Conditions", expanded: true });
+  bcFolder.addBinding(baseParams, "$theta0", {
+    min: -90,
+    max: 90,
+    label: "θ₀",
+    step: 0.0001,
+  });
+  bcFolder.addBinding(baseParams, "$omega0", {
+    label: "ω₀",
+    step: 0.01,
+  });
+  bcFolder.addBinding(baseParams, "$x0perl", {
+    label: "x₀ / l",
+    min: -1,
+    max: 1,
+    step: 0.01,
+  });
+  bcFolder.addBinding(baseParams, "$v0", {
+    label: "v₀",
+    step: 0.01,
+  });
+
+  pane.addButton({
+    title: "Play / Pause",
+  }).on("click", () => {
+    isPlaying = !isPlaying;
+    setIsReset(false);
+  });
+  pane.addButton({
+    title: "Reset",
+  }).on("click", () => {
+    simulator.handleReset();
+    initializeHamiltonianMonitor();
+    setIsReset(true);
+  });
+
+  pane.on("change", (e) => {
+    const binding = unsafeGetBinding<Binding | MonitorBinding>(e);
+    if (!binding) return;
+    switch (binding.key) {
+      case "hamiltonian":
+        break;
+      case "showLocus":
+        simulator.refreshParams();
+        break;
+      default:
+        if (isReset) {
+          simulator.handleReset();
+        } else {
+          simulator.refreshParams();
+        }
+        initializeHamiltonianMonitor();
+    }
+  });
+
   let diagonalLength: number;
   let drawingContext: CanvasRenderingContext2D;
-  const locus: Point2D[] = [];
-
   p.setup = () => {
-    function setIsReset(value: boolean) {
-      isReset = value && !isPlaying;
-      bcFolder.disabled = !isReset;
-      if (value) locus.length = 0;
-    }
     p.createCanvas(p.windowWidth, p.windowHeight, p5.P2D);
     diagonalLength = Math.hypot(p.width, p.height);
     drawingContext = p.drawingContext as CanvasRenderingContext2D;
-
-    const pane = new Pane();
-    const materialFolder = pane.addFolder({ title: "Material", expanded: false });
-    materialFolder.addBinding(baseParams, "l", { min: 0 });
-    materialFolder.addBinding(baseParams, "m", { min: 0 });
-    materialFolder.addBinding(baseParams, "M", { min: 0 });
-
-    const simFolder = pane.addFolder({ title: "Simulation", expanded: false });
-    simFolder.addBinding(baseParams, "dt", { min: 0, max: 1 });
-    simFolder.addBinding(baseParams, "$gravity", { label: "Gravity" });
-    simFolder.addBinding(baseParams, "showLocus", { label: "Show Locus" });
-
-    function initializeHamiltonianMonitor() {
-      const hamiltonianInit = simulator.renderableState.hamiltonian;
-      const { valueController: { props }, value: { rawValue } } = hamiltonianMonitor.controller;
-      props.set("min", hamiltonianInit * 0.9);
-      props.set("max", hamiltonianInit * 1.1);
-      rawValue.fill(undefined);
-    }
-    const hamiltonianOpts = {
-      enabled: true,
-      get hamiltonian() {
-        return simulator.renderableState.hamiltonian;
-      },
-    };
-    simFolder.addBinding(hamiltonianOpts, "enabled", { label: "Monitor Hamiltonian" }).on(
-      "change",
-      () => {
-        hamiltonianMonitor.hidden = !hamiltonianOpts.enabled;
-      },
-    );
-    const hamiltonianMonitor = simFolder.addBinding(hamiltonianOpts, "hamiltonian", {
-      label: "Hamiltonian",
-      readonly: true,
-      view: "graph",
-    }) as GraphLogMonitorBindingApi;
-    initializeHamiltonianMonitor();
-
-    const bcFolder = pane.addFolder({ title: "Boundary Conditions", expanded: true });
-    bcFolder.addBinding(baseParams, "$theta0", {
-      min: -90,
-      max: 90,
-      label: "θ₀",
-      step: 0.0001,
-    });
-    bcFolder.addBinding(baseParams, "$omega0", {
-      label: "ω₀",
-      step: 0.01,
-    });
-    bcFolder.addBinding(baseParams, "$x0perl", {
-      label: "x₀ / l",
-      min: -1,
-      max: 1,
-      step: 0.01,
-    });
-    bcFolder.addBinding(baseParams, "$v0", {
-      label: "v₀",
-      step: 0.01,
-    });
-
-    pane.addButton({
-      title: "Play / Pause",
-    }).on("click", () => {
-      isPlaying = !isPlaying;
-      setIsReset(false);
-    });
-    pane.addButton({
-      title: "Reset",
-    }).on("click", () => {
-      simulator.handleReset();
-      initializeHamiltonianMonitor();
-      setIsReset(true);
-    });
-
-    pane.on("change", (e) => {
-      const binding = unsafeGetBinding<Binding | MonitorBinding>(e);
-      if (!binding) return;
-      switch (binding.key) {
-        case "hamiltonian":
-          break;
-        case "showLocus":
-          simulator.refreshParams();
-          break;
-        default:
-          if (isReset) {
-            simulator.handleReset();
-          } else {
-            simulator.refreshParams();
-          }
-          initializeHamiltonianMonitor();
-      }
-    });
   };
 
   p.draw = () => {
